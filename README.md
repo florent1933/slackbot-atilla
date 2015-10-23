8c52d1f7167402662285b85d3c486291c16da67e
Florent <clapieflor@eisti.eu>
2015-10-24 00:16:15 +0200

    Ready for use

---
+++ README.md
@@ -0,0 +1,18 @@
+# Slackbot for Atilla (menage and talk)
+
+A clever slackbot for Atilla in python3
+
+## Usage
+
+python main.py action hook
+
+**action** :
+- m : for menage
+- t : for talk
+
+**hook** :
+- give the address of the incoming hook
+
+## Installation
+
+Like any other python project
---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+import json
+import random
+import sys
+
+
+# text for different action
+talk_text = "{} doit faire un talk de 10 minutes la semaine prochaine"
+menage_text = "Aujourd'hui est un grand jour, c'est la lotterie du ménage ! \n\nLes grands gagnants sont :\n{}"
+
+# our members for the lottery
+atilliens = ["Flavien", "Valz", "Flo", "Antoine", "Thomas H", "Alexandre T", "Flemme"]
+
+# t for talk or m for menage
+action = sys.argv[1]
+# incoming hook
+hook = sys.argv[2]
+
+
+def get_winner(nb):
+    random.shuffle(atilliens)
+    winner = atilliens[-nb::]
+    return winner
+
+
+if action == "t":
+    t = talk_text
+    w = get_winner(1)
+elif action == "m":
+    t = menage_text
+    w = get_winner(4)
+else:
+    exit()
+
+
+data = {
+    "text": "<!everyone>\n" + t.format(",\n".join(["- " + v for v in w]))
+}
+
+r = requests.post(hook, data = json.dumps(data))
